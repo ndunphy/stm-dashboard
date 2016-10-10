77ccbc38026b0109abefe14c6e86ed88abaaa572
James Clements <jamesvclements@Jamess-MacBook-Pro.local>
2016-10-09 20:14:08 -0400

    remove unused files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,3 @@
-// import React, { Component } from 'react'
-// import logo from '../../images/logo.svg'
-// import './App.css'
-
-// export class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     )
-//   }
-// }
-
 import React, { PropTypes } from 'react'
 import { Router } from 'react-router'
 
