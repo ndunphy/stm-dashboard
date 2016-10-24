1f58d9152c903ef4a998d78cfa96ecd698821b46
James Clements <jamesvclements@Jamess-MacBook-Pro.local>
2016-10-24 13:31:31 -0400

    changed to panels

--- src/views/RunPlacements/RunPlacements.js
+++ src/views/RunPlacements/RunPlacements.js
@@ -1,7 +1,8 @@
 import React, { PropTypes as T } from 'react'
+import {Grid, Row, Col, Button,Table} from 'react-bootstrap'
 import AuthService from '../../utils/AuthService'
+import GradePlacePanel from '../../components/Grade/GradePlacePanel'
 import './RunPlacements.css'
-import {Button,Table} from 'react-bootstrap'
 
 export class RunPlacements extends React.Component {
   static contextTypes = {
@@ -13,73 +14,24 @@ export class RunPlacements extends React.Component {
     profile: T.object
   }
 
-  runAlgorithm(grade) {
-    fetch(process.env.REACT_APP_SERVER_ADDRESS + '/api/placements/' + grade, 
-    {
-      method: 'PUT',
-    }).then((response) => {
-      window.location = '#/placement/' + grade
-    })
-  }
-
-  viewAlgorithm(grade) {
-    window.location = '#/placement/' + grade
-  }
-
-
-  getRunButton(gradeID){
-    return (<Button onClick={ () => this.runAlgorithm(gradeID)}>Run</Button>)
-  }
-  getViewButton(gradeID){
-    return (<Button onClick={ () => this.viewAlgorithm(gradeID)}>View</Button>)
-  }
-
-  getTableRow(gradeID, gradeTitle) {
-    return (<tr>
-            <td>{gradeTitle}</td>
-            <td>{this.getRunButton(gradeID)}</td>
-            <td>{this.getViewButton(gradeID)}</td>
-          </tr>)
-  }
-
   render() {
-    const { profile } = this.props
     return (
     <div className="root">
-      <Table striped bordered condensed hover>
-        <tbody>
-          {this.getTableRow(0, "Kindergarten")}
-          {this.getTableRow(1, "Grade 1")}
-          {this.getTableRow(2, "Grade 2")}
-          {this.getTableRow(3, "Grade 3")}
-          {this.getTableRow(4, "Grade 4")}
-          {this.getTableRow(5, "Grade 5")}
-          {this.getTableRow(6, "Grade 6")}
-          {this.getTableRow(7, "Grade 7")}
-          {this.getTableRow(8, "Grade 8")}
-        </tbody>
-      </Table>
+      <Grid>
+        <Row>
+          {
+            [0, 1, 2, 3, 4, 5, 6, 7, 8].map((grade, i) => {
+              return (
+                <Col lg={2} xs={4} key={i}>
+                  <GradePlacePanel grade={grade}/>
+                </Col>)
+            })
+          }
+        </Row>
+      </Grid>
     </div>
     )
   }
 }
 
 export default RunPlacements
-
-/*<ButtonToolbar bsSize="large" >
-          <Button href="/placement" bsStyle="primary">Kindergarten</Button>
-          <Button bsStyle="primary">Grade 1</Button>
-          <Button bsStyle="primary">Grade 2</Button>
-        </ButtonToolbar>
-        <br/>
-        <ButtonToolbar bsSize="large" >
-          <Button bsStyle="primary">Grade 3</Button>
-          <Button bsStyle="primary">Grade 4</Button>
-          <Button bsStyle="primary">Grade 5</Button>
-        </ButtonToolbar>
-        <br/>
-        <ButtonToolbar bsSize="large" >
-          <Button bsStyle="primary">Grade 6</Button>
-          <Button bsStyle="primary">Grade 7</Button>
-          <Button bsStyle="primary">Grade 8</Button>
-        </ButtonToolbar>*/
