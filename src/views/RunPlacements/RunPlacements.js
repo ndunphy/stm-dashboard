4512b8afad980586e3d2ac776e84ccdaf2260e5f
James Clements <jamesvclements@Jamess-MacBook-Pro.local>
2016-10-24 13:33:00 -0400

    removed unnecessary imports

--- src/views/RunPlacements/RunPlacements.js
+++ src/views/RunPlacements/RunPlacements.js
@@ -1,14 +1,10 @@
 import React, { PropTypes as T } from 'react'
-import {Grid, Row, Col, Button,Table} from 'react-bootstrap'
+import {Grid, Row, Col} from 'react-bootstrap'
 import AuthService from '../../utils/AuthService'
 import GradePlacePanel from '../../components/Grade/GradePlacePanel'
 import './RunPlacements.css'
 
 export class RunPlacements extends React.Component {
-  static contextTypes = {
-    router: T.object
-  }
-
   static propTypes = {
     auth: T.instanceOf(AuthService),
     profile: T.object
